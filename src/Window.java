f8dfe4550257819ddfb2f94c7e547c5c7dc31428
vmataba <vmataba0@gmail.com>
2017-09-26 18:32:52 +0300

    Adding render function in Window.java

--- src/Window.java
+++ src/Window.java
@@ -1,15 +1,42 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by victor on 9/26/17.
  */
 public class Window extends JFrame {
 
+    private JPanel panel = null;
+    private JTextField screen = null;
+    private JButton[] buttons = null;
+    private Font font = null;
+
     public Window(String title){
         this.setTitle(title);
         this.setLocationRelativeTo(null);
         this.setSize(330,500);
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+
+        this.render();
+
         this.setVisible(true);
+    }
+
+    private void render () {
+
+        font = new Font("Tahoma",Font.BOLD,20);
+
+        screen = new JTextField();
+        screen.setPreferredSize(new Dimension(310,50));
+        screen.setBorder(BorderFactory.createSoftBevelBorder(0));
+        screen.setFont(font);
+
+        panel = new JPanel();
+        panel.setBackground(Color.GRAY);
+
+        panel.add(screen);
+        this.add(panel);
+
+
     }
 }
